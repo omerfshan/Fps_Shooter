e5e96c28c32c61ae3cde4f44a1a238cc669c0bf4
sahan <fs.sahan09@gmail.com>
2025-11-12 06:54:04 +0300

    project Start

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,18 @@
+using Unity.Profiling;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    //MouseInputlarını alıp
+    //boody dondurecek 
+    //asagı yukarı kafa donecek
+    [SerializeField] Transform _characterHead;
+    [SerializeField] Transform _characterBody;
+
+    void Update()
+    {
+        
+    }
+
+
+}
